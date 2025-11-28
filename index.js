7a7fd56e2439c9904fffb8298e1cf022c562d713
SSloan07 <159721122+SSloan07@users.noreply.github.com>
2025-11-27 20:45:02 -0500

    Creation of tables of asignatures in html

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express"); 
+const path = require("path");
 const app = express(); 
 const port = 3000; 
 
-app.use(express.json()); // Esto hace que todos los datos que el usuario mande se van a convertir a JSON 
-
+app.use(express.json());
+app.use(express.static("FrontEnd"));
 
 let Notas = [
     {Materia: "Calculo", Nota: 5, Creditos: 3},
-    {Materia: "NFI", Nota: 4, Creditos:3}, 
-    {Materia: "Sistemas Operativos", Nota: 5, Creditos:3},     
+    {Materia: "NFI", Nota: 4, Creditos: 3}, 
+    {Materia: "Sistemas Operativos", Nota: 5, Creditos: 3}     
 ];
+
 app.get("/", (req, res) => {
-    return res.json(Notas); 
-}); 
+    return res.sendFile(path.join(__dirname, "FrontEnd", "Estructura.html"));
+});
+
 app.listen(port, () =>{
-    console.log("Servidor escuchando en http://localhost:"+port); 
-}); 
+    console.log("Servidor escuchando en http://localhost:" + port);
+});
